d658ab43d2437367ee411c7c42a753d370abe440
Benjamin Fiche <benjamin.f@datascientest.com>
2026-02-20 13:14:10 +0100

    Change Message

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello Masterclass Dev !'));
+app.get('/', (req, res) => res.send('Hello Masterclass Prod !'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
